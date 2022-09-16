0ac6b349d9749aa3b7ff772c8299a0dbe3143f32
Héctor Airy <hola@hectorairy.com>
2022-09-16 14:59:43 -0500

    refactor: creates custom hook to save items in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,30 @@ import { Todo } from "./components/Todo";
 //   { text: "Llorar con la llorona", completed: true },
 // ];
 
-function App() {
-  const localStorageTodos = localStorage.getItem("TODOS_V1");
-  let parsedTodos;
-  if (!localStorageTodos) {
-    localStorage.setItem("TODOS_V1", JSON.stringify([]));
-    parsedTodos = [];
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
   } else {
-    parsedTodos = JSON.parse(localStorageTodos);
+    parsedItem = JSON.parse(localStorageItem);
   }
 
-  const [todos, setTodos] = useState(parsedTodos);
+  const [item, setItem] = useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
+
+function App() {
+  const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
   const [searchValue, setSearchValue] = useState("");
 
   let searchedTodos = [];
@@ -32,12 +45,6 @@ function App() {
   }
   const totalTodos = searchedTodos.length;
   const completedTodos = searchedTodos.filter((todo) => todo.completed).length;
-
-  const saveTodos = (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem("TODOS_V1", stringifiedTodos);
-    setTodos(newTodos);
-  };
 
   const onComplete = (text) => {
     const updatedTodos = todos.map((todo) => {
