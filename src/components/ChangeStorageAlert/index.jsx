9c4e457975835c5e26ff255049afa2a7118c035c
Héctor Airy <hola@hectorairy.com>
2022-10-05 00:16:31 -0500

    style: adds new style to ChangeStorageAlert component

--- src/components/ChangeStorageAlert/index.jsx
+++ src/components/ChangeStorageAlert/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { withStorageListener } from "./withStorageListener";
+import "./ChangeStorageAlert.css";
 
 const ChangeStorageAlert = ({ show, synchronize }) => {
   if (show) {
     return (
-      <div>
-        <p>Hubo cambios</p>
-        <button onClick={() => synchronize()}>Refrescar la información</button>
+      <div className="ChangeStorageAlert">
+        <div className="ChangeStorageAlert__container">
+          <p className="ChangeStorageAlert__text">
+            Hemos encontrado cambios nuevos
+          </p>
+          <button
+            className="ChangeStorageAlert__button"
+            onClick={() => synchronize()}
+          >
+            ¡Muestrame lo nuevo!
+          </button>
+        </div>
       </div>
     );
   }
